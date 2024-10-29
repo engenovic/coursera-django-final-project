ac201f5a000bd9854065ac2be2f9a52866c718ad
engenovic <engenovic@gmail.com>
2024-10-29 21:02:35 +0300

    Initial project setup

---
+++ restaurant/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import Menu
+from .models import Booking
+
+# Register your models here.
+admin.site.register(Menu)
+admin.site.register(Booking)
+
+
+
+
+
+
